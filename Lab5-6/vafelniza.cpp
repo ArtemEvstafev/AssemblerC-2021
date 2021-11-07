13deec02494312edd4637205b305e25061e06e9a
ArtemEvstafev <vur1artem1cheb@gmail.com>
2021-11-07 22:56:18 +0300

    так float* tmp не работает(

--- Lab5-6/vafelniza.cpp
+++ Lab5-6/vafelniza.cpp
@@ -6,7 +6,9 @@
 using namespace std;
 int N = 16;
 float suma[4] = {0.0};
-float tmp[4];
+//float tmp[4];
+float* tmp;
+float** tmp1;
 float sum=0.;
 
 int main()
@@ -34,29 +36,30 @@ int main()
         //Засекаем время
         auto start = chrono::high_resolution_clock::now();
         //Сумма элементов
-        for(int i=0; i<n; ++i)
-           sum+=arr[i];
-//        for(int i=0; i< n-4; i+=4)
-//        {
+//        for(int i=0; i<n; ++i)
+//           sum+=arr[i];
+        for(int i=0; i< n-4; i+=4)
+        {
 //            tmp[0] = arr[i];
 //            tmp[1] = arr[i+1];
 //            tmp[2] = arr[i+2];
 //            tmp[3] = arr[i+3];
-//            asm(
-//                "movups tmp(%rip), %xmm0 \n"
-//                "movups suma(%rip), %xmm1 \n"
-//                "addps %xmm0, %xmm1 \n"
-//                "movups %xmm1, suma(%rip) \n"
-//                    );
-//        }
-//        sum = suma[0]+suma[1]+suma[2]+suma[3];
+            tmp = arr+i;
+            asm(
+                "movups tmp(%rip), %xmm0 \n"
+                "movups suma(%rip), %xmm1 \n"
+                "addps %xmm0, %xmm1 \n"
+                "movups %xmm1, suma(%rip) \n"
+                    );
+        }
+        sum = suma[0]+suma[1]+suma[2]+suma[3];
         sum = sum / float(n);
         auto end = chrono::high_resolution_clock::now();
 
         chrono::duration<double> diff = end - start;
         //outfile << n << "          " << diff.count() << endl;
-        cout <<diff.count()<<"\n";
-        //cout <<n<<"\t\t"<<sum<<"\t\t"<<diff.count() << endl;
+        //cout <<diff.count()<<"\n";
+        cout <<n<<"\t\t"<<sum<<"\t\t"<<diff.count() << endl;
         delete [] arr;
         sum = 0.;
         suma[0] = 0.;
